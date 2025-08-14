e52f5fc7546c416f1222874fde641d0ffd81c571
David Hurrell <dhurrell@hotmail.co.uk>
2025-08-14 19:30:57 +0100

    Add UFOMap component to display UFO sightings on a map with location-based filtering

---
+++ components/UFOMap.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+import DisplaySightingModal from "./DisplaySightingModal";
+export default function UFOMap({ toggleLocation, filteredSightings }) {
+  let latitude;
+  let longitude;
+  let latDelta;
+  let longDelta;
+  const [sightingModalVisible, setSightingModalVisible] = React.useState(false);
+  const [selectedSighting, setSelectedSighting] = React.useState(null);
+  console.log("Toggle Location:", toggleLocation);
+  if (toggleLocation == "UK") {
+    latitude = 54.5; // Rough center of the UK
+    longitude = -3.5; // Rough center of the UK
+    latDelta = 8; // Bigger delta to zoom out
+    longDelta = 8; // Bigger delta to zoom out
+  } else if (toggleLocation == "US") {
+    (latitude = 38.30558200233986), -99.74779056184356; // Rough center of the US (San Francisco)
+    longitude = -99.74779056184356; // Rough center of the US (San Francisco)}
+    latDelta = 50; // Bigger delta to zoom out
+    longDelta = 50; // Bigger delta to zoom out
+  }
+  function handleMarkerPress(sighting) {
+    console.log("Marker pressed:", sighting);
+    setSelectedSighting(sighting);
+    setSightingModalVisible(true);
+  }
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude:
+            filteredSightings.length > 0
+              ? parseFloat(filteredSightings[0].latitude)
+              : latitude,
+          longitude:
+            filteredSightings.length > 0
+              ? parseFloat(filteredSightings[0].longitude)
+              : longitude,
+          latitudeDelta: latDelta,
+          longitudeDelta: longDelta,
+        }}
+      >
+        {filteredSightings.map((sighting, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: parseFloat(sighting.latitude),
+              longitude: parseFloat(sighting.longitude),
+            }}
+            title={sighting.title}
+            description={sighting.description}
+            onPress={() => {
+              handleMarkerPress(sighting);
+            }}
+          />
+        ))}
+      </MapView>
+      {sightingModalVisible && (
+        <DisplaySightingModal
+          selectedSighting={selectedSighting}
+          sightingModalVisible={sightingModalVisible}
+          setSightingModalVisible={setSightingModalVisible}
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // width: "90%",
+    alignItems: "center",
+    // paddingTop: 10,
+    // paddingBottom: 10,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: "#39FF14", // neon green border
+    marginTop: 20,
+    height: 500,
+  },
+  map: {
+    flex: 1,
+    width: "100%",
+    height: 500,
+  },
+});
