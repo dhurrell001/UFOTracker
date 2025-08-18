59261fd04fe604cac9b838856a73d68bb7a5c6c9
David Hurrell <dhurrell@hotmail.co.uk>
2025-08-18 17:24:47 +0100

    Remove unused TestAsset component for loading UFO CSV asset

--- components/csvTest.js
+++
@@ -1,8 +0,0 @@
-import { Asset } from "expo-asset";
-
-export default function TestAsset() {
-  console.log("Loading asset...");
-  const asset = Asset.fromModule(require("../assets/ufoTwo.csv"));
-  console.log("Asset:", asset);
-  return null;
-}
